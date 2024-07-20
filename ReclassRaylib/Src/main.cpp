3a9584b84d969dde81493b11f0651066b25e2fbf
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-07-20 15:16:14 +0200

    ImGui

--- ReclassRaylib/Src/main.cpp
+++ ReclassRaylib/Src/main.cpp
@@ -7,17 +7,25 @@
 #include <raylib.h>
 #include <iostream>
 #include "../ImRaylib/rlImgui.hpp"
+#include "../ImGui/imgui.h"
 
 int main(int argc, const char * argv[]) {
     InitWindow(800, 600, "Hello from MacOS !");
     SetTargetFPS(60);
-    
+    rlImGuiSetup(true);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        
+        rlImGuiBegin();
+        ImGui::Begin("Hello world !");
+        ImGui::Text("Hii");
+        ImGui::End();
+        rlImGuiEnd();
         EndDrawing();
     }
+    rlImGuiShutdown();
     CloseWindow();
     
     return 0;
